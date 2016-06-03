d6d7c1d5311496dc5d96c3687901843addd1bc51
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-06-03 19:32:32 +0800

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,6 +4,6 @@ var token = randomstring.generate();
 var fs = require('fs');
 
 var contents = fs.readFileSync(path.join(__dirname, '..', '.env.example'), 'utf8').replace('RANDOM STRING HERE', token);
-fs.writeFileSync('.castmydata.env', contents);
+fs.writeFileSync(path.join('..', '.castmydata.env'), contents);
 
 console.log('Done... API Token is: ' + token);
