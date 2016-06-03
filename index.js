a23cc2b281113fc18a2d21260366dffebdf6f6f4
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-06-03 18:59:50 +0800

    Major code refactoring

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var castmydata = require('./lib/castmydata');
 
-// The express castmydata is exposed 
+// The express instance is exposed 
 castmydata.api.use(function(req, res, next){
     // console.log('express middleware');
     next();
 });
 
-// And so is the socket.io castmydata
+// And so is the socket.io instance
 castmydata.io.use(function(socket, next){
     // console.log('socket.io middlware');
     next();
 });
 
-// And so is the http castmydata
+// And so is the http instance
 castmydata.http.on('connect', function(){
   // do something
 });
