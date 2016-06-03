29c11b448c80a251f2d16616fedb9fe3a8c657a8
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-06-03 18:21:00 +0800

    Renamed listen function to start to reduce ambiguity

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var server = require('./lib/server');
 
 // The express server is exposed 
-
 server.api.use(function(req, res, next){
     // console.log('express middleware');
     next();
 });
 
 // And so is the socket.io server
-
 server.io.use(function(socket, next){
     // console.log('socket.io middlware');
     next();
 });
 
-server.listen();
+// And so is the http server
+server.http.on('connect', function(){
+  // do something
+});
+
+// OK LETS GO!
+server.start();
