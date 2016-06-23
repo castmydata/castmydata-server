a1cc423a16c7e4ef9e6bc2c1bca32eba60cece2b
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-06-23 18:14:50 +0800

    Add stop method

--- castmydata.js
+++ castmydata.js
@@ -9,11 +9,15 @@ var io = require('./lib/io')(http.server);
 // Startup script
 var start = require('./lib/start')(http, io);
 
+// Shutdown script
+var stop = require('./lib/stop')(http);
+
 // Use middleware
 var use = require('./lib/use')(http, io);
 
 module.exports = {
     start: start,
+    stop: stop,
     api: http.api,
     http: http.server,
     io,
